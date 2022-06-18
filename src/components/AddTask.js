3e9ddd315eabeb873cd924442a2ff9a00d4be521
AdrianOrtiga <adrian.ortiga@gmail.com>
2022-06-18 16:08:13 +0800

    fix addTaks was used before being define

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,46 +1,46 @@
 import { useState } from "react"
 
-export default function AddTask({ addTask={addTask} }) {
-    const [text, setText] = useState('')
-    const [date, setDate] = useState('')
-    const [reminder, setReminder] = useState(false)
+export default function AddTask ({ addTask }) {
+  const [text, setText] = useState('')
+  const [date, setDate] = useState('')
+  const [reminder, setReminder] = useState(false)
 
-    function handleSubmit(e){
-        e.preventDefault()
+  function handleSubmit (e) {
+    e.preventDefault()
 
-        if(!text) {
-            alert('Please add task')
-            return
-        }
-        
-        if(!date) {
-            alert('Please add date')
-            return
-        }
-
-        addTask({text:text, date:date,reminder:reminder})
-
-        setText('')
-        setDate('')
-        setReminder(false)
+    if (!text) {
+      alert('Please add task')
+      return
     }
 
-    return (
-        <form className="add-form" onSubmit={handleSubmit}>
-            <div className="form-control">
-                <label>Task</label>
-                <input type='text' placeholder="Add task" value={text} onChange={(e) => setText(e.target.value)}/>
-            </div>
-            <div className="form-control">
-                <label>Date and Time</label>
-                <input type='datetime-local' placeholder="Date and time" value={date} onChange={(e) => setDate(e.target.value)}/>
-            </div>
-            <div className="form-control form-control-check">
-                <label>Reminder</label>
-                <input type='checkbox' checked={reminder} onChange={(e) => setReminder(e.target.checked)}/>
-            </div>
+    if (!date) {
+      alert('Please add date')
+      return
+    }
 
-            <input type='submit' value='Save Task' className='btn btn-block' />
-        </form>
-    )
+    addTask({ text: text, date: date, reminder: reminder })
+
+    setText('')
+    setDate('')
+    setReminder(false)
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <div className="form-control">
+        <label>Task</label>
+        <input type='text' placeholder="Add task" value={text} onChange={(e) => setText(e.target.value)} />
+      </div>
+      <div className="form-control">
+        <label>Date and Time</label>
+        <input type='datetime-local' placeholder="Date and time" value={date} onChange={(e) => setDate(e.target.value)} />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Reminder</label>
+        <input type='checkbox' checked={reminder} onChange={(e) => setReminder(e.target.checked)} />
+      </div>
+
+      <input type='submit' value='Save Task' className='btn btn-block' />
+    </form>
+  )
 }
